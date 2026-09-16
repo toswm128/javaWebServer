f9ba8191444c2bb0015663d5039e590a30f3e018
minsu <chominsu10@gmail.com>
2026-09-16 19:54:52 +0900

    Rotuer pathValue 검색 객체화

--- src/main/java/http/HttpRequestParser.java
+++ src/main/java/http/HttpRequestParser.java
@@ -23,30 +23,34 @@ public class HttpRequestParser {
                 break;
             }
         }
+        try {
 
-        String[] headerLines = buffer.toString().split("\r\n");
-        String[] requestParts = headerLines[0].split(" ");
-        String method = requestParts[0];
-        String path = requestParts[1];
-        for (String header : headerLines) {
-            if (header.contains(":")) {
-                int colonIndex = header.indexOf(':');
-                if (colonIndex >= 0) {
-                    headers.set(header.substring(0, colonIndex), header.substring(colonIndex + 1).trim());
+            String[] headerLines = buffer.toString().split("\r\n");
+            String[] requestParts = headerLines[0].split(" ");
+            String method = requestParts[0];
+            String path = requestParts[1];
+            for (String header : headerLines) {
+                if (header.contains(":")) {
+                    int colonIndex = header.indexOf(':');
+                    if (colonIndex >= 0) {
+                        headers.set(header.substring(0, colonIndex), header.substring(colonIndex + 1).trim());
+                    }
                 }
             }
+
+            int contentLength = 0;
+            String contentLengthHeader = headers.get("Content-Length");
+
+            if (contentLengthHeader != null) {
+                contentLength = Integer.parseInt(contentLengthHeader);
+            }
+            byte[] bodyBytes = input.readNBytes(contentLength);
+
+            return new HttpRequest(method, path, headers, new HttpBody(new String(bodyBytes, StandardCharsets.UTF_8)));
+
+        } catch (Exception e) {
+            return null;
         }
-
-        int contentLength = 0;
-        String contentLengthHeader = headers.get("Content-Length");
-
-        if (contentLengthHeader != null) {
-            contentLength = Integer.parseInt(contentLengthHeader);
-        }
-        byte[] bodyBytes = input.readNBytes(contentLength);
-
-        return new HttpRequest(method, path, headers, new HttpBody(new String(bodyBytes, StandardCharsets.UTF_8)));
-
     }
 
 
--- src/main/java/router/Router.java
+++ src/main/java/router/Router.java
@@ -2,9 +2,7 @@ package router;
 
 import http.Handler;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class Router {
     public record RouteMatch(
@@ -13,40 +11,68 @@ public class Router {
     ) {
     }
 
+    public record PathSegment(
+            String path,
+            boolean variable,
+            String valueName
+    ) {
+
+    }
+
+    private List<PathSegment> parsePathSegments(String path) {
+        List<PathSegment> pathList = new ArrayList<>();
+        for (String p : path.trim().split("/")) {
+            String pathPiece = p.trim();
+            String valueName = null;
+            boolean variable = false;
+            if (pathPiece.startsWith("{") && pathPiece.endsWith("}")) {
+                valueName = pathPiece.substring(1, pathPiece.length() - 1);
+                variable = true;
+            }
+            pathList.add(new PathSegment(pathPiece, variable, valueName));
+        }
+
+
+        return pathList;
+    }
+
     private final Map<String, Handler> routes = new HashMap<>();
+    private final Map<String, List<PathSegment>> pathLists = new HashMap<>();
 
     public void get(String path, Handler handler) {
-        routes.put("GET " + path, handler);
+        String methodPath = "GET " + path;
+        pathLists.put(methodPath, parsePathSegments(methodPath));
+        routes.put(methodPath, handler);
     }
 
     public void post(String path, Handler handler) {
-        routes.put("POST " + path, handler);
+        String methodPath = "POST " + path;
+        pathLists.put(methodPath, parsePathSegments(methodPath));
+        routes.put(methodPath, handler);
     }
 
     public RouteMatch find(String method, String path) {
         if (routes.get(method + " " + path) == null) {
-            String methodPath = method + " " + path;
-            for (String s : routes.keySet()) {
-                String[] routedPath = s.split("/");
-                String[] routingPath = methodPath.split("/");
-                if (routedPath.length == routingPath.length) {
-                    Map<String, String> pathMap = CheckPath(routedPath, routingPath);
-                    if (pathMap != null) {
-                        return new RouteMatch(routes.get(s), pathMap);
-                    }
+            List<PathSegment> routingPathList = parsePathSegments(method + " " + path);
+
+            for (String listKey : pathLists.keySet()) {
+                System.out.println(listKey + "  " + pathLists.get(listKey) + "  " + Arrays.asList(routingPathList));
+                if (routingPathList.size() == pathLists.get(listKey).size()) {
+                    Map<String, String> pathMap = CheckPath(pathLists.get(listKey), routingPathList);
+                    if (pathMap != null)
+                        return new RouteMatch(routes.get(listKey), pathMap);
                 }
             }
         }
         return new RouteMatch(routes.get(method + " " + path), null);
     }
 
-    private Map<String, String> CheckPath(String[] routedPath, String[] routingPath) {
+    private Map<String, String> CheckPath(List<PathSegment> routedPath, List<PathSegment> routingPath) {
         Map<String, String> pathMap = new HashMap<>();
-
-        for (int i = 0; routedPath.length > i; i++) {
-            if (routedPath[i].trim().startsWith("{") && routedPath[i].trim().endsWith("}")) {
-                pathMap.put(routedPath[i].trim().substring(1, routedPath[i].trim().length() - 1), routingPath[i].trim());
-            } else if (!Objects.equals(routedPath[i], routingPath[i])) {
+        for (int i = 0; routedPath.size() > i; i++) {
+            if (routedPath.get(i).variable()) {
+                pathMap.put(routedPath.get(i).valueName(), routingPath.get(i).path());
+            } else if (!Objects.equals(routedPath.get(i).path(), routingPath.get(i).path())) {
                 return null;
             }
         }
