dec77f70e024d845c72ab4e2375f5a114c3fab34
조민수 <63090043+toswm128@users.noreply.github.com>
2026-09-27 01:00:39 +0900

    jdbc 찍먹

--- src/main/java/http/Handler.java
+++ src/main/java/http/Handler.java
@@ -1,10 +1,11 @@
 package http;
 
+import java.sql.SQLException;
 import java.util.Map;
 
 public interface Handler {
 
   HttpResponse handle(HttpRequest httpRequest, Map<String, String> pathValue)
-      throws InterruptedException;
+      throws InterruptedException, SQLException;
 }
 
--- src/main/java/user/UserRoutes.java
+++ src/main/java/user/UserRoutes.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import http.HttpResponse;
 import http.HttpStatus;
 import http.Json;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,8 +25,36 @@ public class UserRoutes {
 
   static final ObjectMapper objectMapper = new ObjectMapper();
 
+  private static List<User> findAllUsers() throws SQLException {
+    String url = System.getenv("DB_URL");
+    String user = System.getenv("DB_USER");
+    String password = System.getenv("DB_PASSWORD");
+    List<User> userList = new ArrayList<>();
+
+    try (Connection connection = DriverManager.getConnection(url, user, password);
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users");
+        ResultSet resultSet = preparedStatement.executeQuery();
+    ) {
+      System.out.println("디비 접속");
+      while (resultSet.next()) {
+        userList.add(
+            new User(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getInt("age")));
+      }
+    }
+
+    return userList;
+  }
+
   public static void register(Router router) {
     router.get("/users", (request, __) -> {
+      List<User> userList;
+      try {
+        userList = findAllUsers();
+
+      } catch (SQLException e) {
+        System.out.println(e.getMessage());
+        return HttpResponse.text(HttpStatus.INTERNAL_SERVER_ERROR, e.getSQLState());
+      }
       String json = Json.write(userList);
       return HttpResponse.text(
           HttpStatus.OK,
